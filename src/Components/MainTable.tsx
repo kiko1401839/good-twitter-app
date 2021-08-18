21b86e2dde2face17a403a6002259a85caf24e5d
kiko1401839 <kyamasaki95624@gmail.com>
2021-08-18 20:53:34 +0900

    Add function to get data by TwitterAPI [#3]

--- src/Components/MainTable.tsx
+++ src/Components/MainTable.tsx
@@ -1,21 +1,52 @@
+import axios from 'axios';
 import React from 'react';
 import InputForm from './InputForm';
 
+
+type typeImages = {
+    url: string[];
+    height: string[];
+    source: string[];
+    max_id: string;
+};
+
 type typeImageTableState = {
+    images: typeImages;
     message: string;
-}
+    screen_name: string;
+};
+
 
 class MainTable extends React.Component<{}, typeImageTableState> {
     constructor(props: {}) {
         super(props);
         this.state = {
+            images: {
+                url: [],
+                height: [],
+                source: [],
+                max_id: ''
+            },
             message: '',
+            screen_name: ''
         };
     }
 
     handleSubmit = (screen_name: string) => {
-        this.setState({message: 'done'});
-        console.log(screen_name);
+        twitterAPI(screen_name, this.state.images.max_id)
+            .then(res => {
+                this.setFavoriteImages(res);
+            })
+            .catch(() => {
+                this.setState({
+                    message: 'Failed to get data: Incorrect screen name or empty data.'
+                })
+            })
+    }
+
+    setFavoriteImages = (results: any) => {
+        this.setState({images: results, message: 'done'});
+        console.log(this.state.images);
     }
 
     render() {
@@ -31,3 +62,16 @@ class MainTable extends React.Component<{}, typeImageTableState> {
 }
 
 export default MainTable;
+
+const twitterAPI = (screen_name: string, max_id: string) => {
+    const endpoint = `${process.env.REACT_APP_API_ENDPOINT_URL}?name=${screen_name}&max_id=${max_id}`;
+    return new Promise((resolve, reject) => {
+        axios.get(endpoint)
+            .then(res => {
+                resolve(res.data);
+            })
+            .catch(err => {
+                reject(err);
+            });
+    });
+}
